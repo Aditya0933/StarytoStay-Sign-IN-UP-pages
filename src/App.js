3698aa561d85d9f5e6aaae7f33209af3bcc7fea4
Aditya Singh Parihar <201037@kit.ac.in>
2023-08-22 19:29:58 +0530

    Adding Rescue Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import SignUP from './components/SignUP';
 import SignIN from './components/SignIN';
+import Rescue from './components/Rescue';
 
 function App() {
   return (
     <div className="App flex flex-col">
       <SignUP/>
       <SignIN/>
+      <Rescue/>
     </div>
   );
 }
---
+++ src/components/Rescue.jsx
@@ -0,0 +1,192 @@
+
+import { Box, Stack, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import injuredDog from '../assets/rescue.webp'
+import helpDog from '../assets/help-rescue-background-img.jpeg'
+import BoraBora from '../assets/help-rescue-background-img.jpeg';
+import BoraBora2 from '../assets/help-rescue-background-img.jpeg';
+import Maldives from '../assets/help-rescue-background-img.jpeg';
+import Maldives2 from '../assets/help-rescue-background-img.jpeg';
+import Maldives3 from '../assets/help-rescue-background-img.jpeg';
+import KeyWest from '../assets/help-rescue-background-img.jpeg';
+import {
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  TextareaAutosize,
+  Button,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  FormLabel
+} from '@mui/material';
+ 
+const initialFormData = {
+  name: '',
+  type: '',
+  injuries: '',
+  injuryDetail: '',
+  breed: '',
+  address: '',
+  city: '',
+  country: '',
+  photo: '',
+};
+
+const petTypes = [
+  'dog',
+  'cat',
+  'bird',
+  'cow',
+  'rabbit',
+  'horse',
+  'goat',
+  'pig',
+  'other',
+];
+
+function Rescue() {
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  return (
+    <div>
+      <Box width={{lg:'80%',md:'100%',xs:'100%'}} mx={'auto'}>
+        <Box  className='rescue-home'   sx={{ p: 2.9 ,backgroundSize:'cover' , backgroundRepeat:'no-repeat',backgroundColor: 'rgba(0, 0, 0, 0.5)', backgroundImage:`url(${helpDog})`}}>
+          <Stack justifyContent='end' alignItems='center'>
+              <Box minHeight='100vh' display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'}>
+                <Typography fontSize={{md:58,sm:48,xs:42}} fontWeight={700} color='#ffff' lineHeight={1.5} letterSpacing={1}> They Need Your Help, </Typography>
+                <Typography fontSize={{md:58,sm:48,xs:38}} fontWeight={700} color='#ffff' marginTop={2} lineHeight={1.3} letterSpacing={1}> Love & Support </Typography>
+                <Button className='rescue-btn' type="submit" justifyContent='center' variant="contained"  sx={{color:'white',backgroundColor:'#ff6d00',fontSize:'24px',marginTop:'20px'}}>
+                  RESCUE THEM 
+                </Button>
+              </Box>  
+          </Stack>
+        </Box>
+        <div className='rescue-content bg-black'>
+        <h1 className='rescue-content-heading'>Their lives matter. Rescue animals</h1>
+        <p className='rescue-content-para'>Animals suffer everyday. Whether it is dehydration, starvation, or lack of attention, these are all leading factors into animal abuse. Animal abuse occurs all around the world. Typically it is something people will turn a blind eye towards because it is not a human life being threatened; it is simply ‘just an animal life.’ Animals are crucial for our universe, not to mention they are capable of being lifelong companions.</p>
+          <div className='max-w-[1240px] mx-auto px-4 py-5 grid sm:grid-cols-2 lg:grid-cols-3 gap-4 bg-black'>
+            <div img={Maldives3} text='Romo' ><p>1</p>
+            <img src={Maldives3} alt="" srcset="" /></div>
+            <div img={BoraBora} text='Buddy'><p>2</p>
+            <img src={Maldives3} alt="" srcset="" /></div>
+            <div img={BoraBora2} text='Max'><p>3</p>
+            <img src={Maldives3} alt="" srcset="" /></div>
+            <div img={Maldives} text='Charlie'><p>4</p>
+            <img src={Maldives3} alt="" srcset="" /></div>
+            <div img={Maldives2} text='Diasy'><p>5</p>
+            <img src={Maldives3} alt="" srcset="" /></div>
+            <div img={KeyWest} text='Druster'><p>6</p>
+            <img src={Maldives3} alt="" srcset="" /></div>  
+          </div>
+        </div>
+        <div>
+        <div>
+          <div className="flex items-center justify-center ">
+            <div className="bg-white">
+                <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+                    <h2 className="text-2xl font-semibold">
+                        Frequently Asked Questions
+                    </h2>
+                    <p className="mt-4 mb-8 text-gray-600">
+                        React Interview Questions And Answers
+                    </p>
+                    <div className="space-y-4">
+                        <details className="w-full rounded-lg ring-1 ring-purple-600">
+                            <summary className="px-4 py-6">
+                                What is React? 
+                            </summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">
+                                React is a front-end JavaScript library
+                                developed by Facebook in 2011. 
+                            </p>
+                        </details>
+                        <details className="w-full rounded-lg ring-1 ring-purple-600">
+                            <summary className="px-4 py-6">
+                            What is Props and how to use it give some example?
+                            </summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">
+                            Props is the shorthand for Properties in React.
+                            </p>
+                        </details>
+                        <details className="w-full rounded-lg ring-1 ring-purple-600">
+                            <summary className="px-4 py-6">
+                            How to install tailwind css in react js ?
+                            </summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">
+                            What are synthetic events in React?
+                            </p>
+                        </details>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        <Box   sx={{  p: 2.9 ,backgroundSize:'cover',  boxSizing: 'content-box', backgroundRepeat:'no-repeat', backgroundImage:`url(${injuredDog})`}}>
+        <Stack justifyContent='end' alignItems='center'>
+            <Box>
+              <Typography fontSize={{md:52,sm:42,xs:32}} fontWeight={700} color='#ffff' lineHeight={1.2} letterSpacing={1}>What to do when you find an injured stray animal !!!</Typography>
+            </Box>  
+        </Stack>
+        <Box width='90%' mx='auto' my={4} ><hr style={{height:'2px',border:'none', backgroundColor:'#e0e0e0'}}/> </Box>
+        <Box width={{md:'70%',xs:'90%'}} mx='auto' mb={4}>
+          <Box sx={{borderLeft:'3px solid #ff6d00',borderRight:'3px solid #ff6d00'}} mb={3}>
+            <Typography color='#000000' fontSize={{sm:44,xs:32}} fontWeight={700} ml={2}>
+                Rescue Form
+            </Typography>
+          </Box>
+          <form style={{display:'flex',flexDirection:'column',justifyContent:'center'}}>
+            <TextField label="Name" name="name" required fullWidth style={{ marginBottom: '15px' }} />
+              <FormControl fullWidth style={{ marginBottom: '15px' }} required>
+                <InputLabel>Type</InputLabel>
+                <Select
+                  label="Type"
+                  name="type"
+                  value={formData.type}
+                  required
+                >
+                  {petTypes.map((type) => (
+                    <MenuItem key={type} value={type}>
+                      {type}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              {formData.type === 'other' && (
+                <TextField label="Type (Other)" name="type" required fullWidth style={{ marginBottom: '15px' }} />
+              )}
+            <TextField label="Breed" name="breed" required fullWidth style={{ marginBottom: '15px' }} />
+            <FormControl style={{ marginBottom: '15px' }}>
+              <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="gender"
+              >
+                <FormControlLabel value="female" control={<Radio />} label="Female" color='#ff6d00'/>
+                <FormControlLabel value="male" control={<Radio />} label="Male" color='#ff6d00'/>
+                <FormControlLabel value="other" control={<Radio />} label="Other" color='#ff6d00'/>
+              </RadioGroup>
+            </FormControl>
+            <TextField label="Injuries" name="injuries"  required fullWidth style={{ marginBottom: '15px' ,color:'#ff6d00'}} />
+            <TextField label="Injury Detail" name='injuryDetail' placeholder="Describe the injuries in detail" required multiline rows={5} maxRows={10} style={{ marginBottom: '15px' }} />
+            <TextField label="Address" name="address" required fullWidth style={{ marginBottom: '15px' }} />
+            <TextField label="City" name="city" required fullWidth style={{ marginBottom: '15px' }} />
+            <TextField label="Country" name="country" required fullWidth style={{ marginBottom: '15px' }} />
+            <TextField label="Photo URL" name="photo" required fullWidth style={{ marginBottom: '15px' }} />
+            <Button type="submit" justifyContent='center' size='large' variant="contained"  sx={{color:'white',backgroundColor:'#ff6d00'}}>
+              Submit
+            </Button>
+          </form>
+        </Box>
+        </Box>
+      </Box>
+    </div>
+  )
+}
+
+export default Rescue;
